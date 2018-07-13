2f870ae7fb97ab2dedf48ca4839b1bef0c71e043
jwarren <warren.jonathan@gmail.com>
2018-07-13 17:23:09 +0100

    added some more endpoints to get dropdowns for heatmaps

--- src/main/java/uk/ac/ebi/threei/rest/controllers/DataController.java
+++ src/main/java/uk/ac/ebi/threei/rest/controllers/DataController.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.apache.solr.client.solrj.SolrServerException;
 import org.json.JSONArray;
@@ -20,6 +23,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import uk.ac.ebi.threei.rest.CellParameter;
+import uk.ac.ebi.threei.rest.CellParameterRepository;
 import uk.ac.ebi.threei.rest.Data;
 import uk.ac.ebi.threei.rest.DataRepository;
 import uk.ac.ebi.threei.rest.services.GeneDTO;
@@ -31,6 +36,12 @@ public class DataController {
 	GeneService geneService;
 	@Autowired
 	DataRepository dataRepo;
+	@Autowired
+	CellParameterRepository cellRepo;
+	
+	private SortedSet<String>uniqueSubCellTypes;
+	private SortedSet<String> uniqueAssays;
+	private SortedSet<String> uniqueCellTypes;
 	
 	@CrossOrigin(origins = "*", maxAge = 3600)
 	@RequestMapping("/data")
@@ -49,6 +60,80 @@ public class DataController {
 		}
 		return new ResponseEntity<Data>(data, HttpStatus.OK);
 	}
+	
+	@CrossOrigin(origins = "*", maxAge = 3600)
+	@RequestMapping("/cellTypes")
+	@ResponseBody
+	public HttpEntity<Set<String>> cellTypes(Model model) {
+		System.out.println("calling get cell subtypes");
+		
+		if(uniqueCellTypes==null) {
+			uniqueCellTypes=new TreeSet<>();
+		//should extract these into methods in a data service for unit testing purposes
+		List<CellParameter> dataList = cellRepo.findAll();
+		
+		for(CellParameter cellP: dataList) {
+			uniqueCellTypes.add(cellP.getCellType());
+		}
+		}
+		return new ResponseEntity<Set<String>>(uniqueCellTypes, HttpStatus.OK);
+	}
+	
+	@CrossOrigin(origins = "*", maxAge = 3600)
+	@RequestMapping("/cellSubTypes")
+	@ResponseBody
+	public HttpEntity<Set<String>> cellSubTypes(Model model) {
+		System.out.println("calling get cell subtypes");
+		
+		if(uniqueSubCellTypes==null) {
+			uniqueSubCellTypes=new TreeSet<>();
+		//should extract these into methods in a data service for unit testing purposes
+		List<CellParameter> dataList = cellRepo.findAll();
+		
+		for(CellParameter cellP: dataList) {
+			uniqueSubCellTypes.add(cellP.getCellSubtype());
+		}
+		}
+		return new ResponseEntity<Set<String>>(uniqueSubCellTypes, HttpStatus.OK);
+	}
+	
+	@CrossOrigin(origins = "*", maxAge = 3600)
+	@RequestMapping("/assays")
+	@ResponseBody
+	public HttpEntity<Set<String>> assays(Model model) {
+		System.out.println("calling get cell subtypes");
+		
+		if(uniqueAssays==null) {
+			uniqueAssays=new TreeSet<String>();
+		//should extract these into methods in a data service for unit testing purposes
+		List<CellParameter> dataList = cellRepo.findAll();
+		
+		for(CellParameter cellP: dataList) {
+			uniqueAssays.add(cellP.getAssay());
+		}
+		}
+		return new ResponseEntity<Set<String>>(uniqueSubCellTypes, HttpStatus.OK);
+	}
+	
+	@CrossOrigin(origins = "*", maxAge = 3600)
+	@RequestMapping("/constructs")
+	@ResponseBody
+	public HttpEntity<List<String>> constructController(Model model, @RequestParam(value = "heatmapType", required = false, defaultValue="procedure") String heatmapType) {
+		System.out.println("calling data controller with heatmapType"+ heatmapType);
+		List<String>uniqueConstructs=new ArrayList<>();
+		//should extract these into methods in a data service for unit testing purposes
+		List<Data> dataList = dataRepo.findAll();
+		Data data = new Data();
+		if(heatmapType.equals("cell")){
+			data=dataList.get(1);
+			
+		}else if(heatmapType.equals("procedure")){
+			data=dataList.get(0);
+		}
+		return new ResponseEntity<List<String>>(uniqueConstructs, HttpStatus.OK);
+	}
+	
+	
 	
 //	@ExceptionHandler()
 //    public void handleException() {
