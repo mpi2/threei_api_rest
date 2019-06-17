c16c3bac131b049b5ba52923d81696952f044b10
jwarren <warren.jonathan@gmail.com>
2019-06-17 16:54:44 +0100

    added new data download links - but ftp site not working??

--- src/main/app-ui/src/app/app.component.ts
+++ src/main/app-ui/src/app/app.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
+import { NgcCookieConsentService } from 'ngx-cookieconsent';
 
-
+const cookieConfig:NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'localhost' // or 'your.domain.com'
+// it is mandatory to set a domain, for cookies to work properly (see https://goo.gl/S2Hy2A)
+  },
+  palette: {
+    popup: {
+      background: '#000'
+    },
+    button: {
+      background: '#f1d600'
+    }
+  },
+  theme: 'edgeless',
+  type: 'opt-out'
+};
 
 @Component({
   selector: 'app-root',
@@ -9,10 +25,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-
-  
-
-  
 
 
 }
