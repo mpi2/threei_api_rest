e6c6fd03657880e235dad17f5e8432e6175a1e64
jwarren <warren.jonathan@gmail.com>
2020-05-07 11:05:28 +0100

    threei covid last version

---
+++ src/main/java/uk/ac/ebi/threei/rest/services/human/CommonMethods.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.threei.rest.services.human;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.ac.ebi.threei.rest.Data;
+
+public class CommonMethods {
+
+	public static  Data addCellData(Data data, int columnI, int rowI, int value) {
+		List<Integer> cellData = new ArrayList<>();
+		cellData.add(columnI);
+		cellData.add(rowI);
+		cellData.add(value);
+		data.getData().add(cellData);
+		return data;
+//		CellData cell=new CellData();
+//		cell.setColumnI(columnI);
+//		cell.setRowI(rowI);
+//		cell.setValue(value);
+//		celldata.getCellData().add(cell);
+		//System.out.println("adding cell data="+cellData);
+	}
+	
+}
